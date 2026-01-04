f41f80bb3a9f512772c4088698c2eebc1f7dd052
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-04 19:19:55 +0530

    Initial Commit

---
+++ LeetCode/src/P121.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class P121 {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buyPrice)
+                buyPrice = prices[i];
+            else if (prices[i] - buyPrice > profit)
+                profit = prices[i] - buyPrice;
+        }
+        return profit;
+    }
+}
