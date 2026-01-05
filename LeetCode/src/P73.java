831ba039091ea6cc8cf89152eb7b4950c174330e
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-05 13:02:42 +0530

    Set Matrix Zeros

---
+++ LeetCode/src/P73.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class P73 {
+    public static void main(String[] args) {
+        int[][] matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        Set<Integer> rowSet = new HashSet<>();
+        Set<Integer> colSet = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+        for (int value : rowSet) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[value][i] = 0;
+            }
+        }
+
+        for (int value : colSet) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][value] = 0;
+            }
+        }
+    }
+}
