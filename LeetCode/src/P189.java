b505ce2292d1c71930b61d522857a77be41b0426
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-06 13:58:41 +0530

    Rotate Array

---
+++ LeetCode/src/P189.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class P189 {
+    public static void main(String[] args) {
+        int[] nums = {1};
+        rotate(nums, 0);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] rotated = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotated[i];
+        }
+    }
+}
