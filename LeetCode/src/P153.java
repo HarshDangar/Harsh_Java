c1efbb5827cc826196e256528bfa3d17cbacd5a1
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-07 11:38:50 +0530

    Min element in rotated sorted array

---
+++ LeetCode/src/P153.java
@@ -0,0 +1,30 @@
+public class P153 {
+    public static void main(String[] args) {
+        int[] nums = {11,13,15,17};
+        System.out.println(findMin(nums));
+    }
+
+    public static int findMin(int[] nums) {
+        int pivot = findPivot(nums);
+
+        if (pivot == -1)
+            return nums[0];
+        else
+            return nums[pivot + 1];
+    }
+
+    public static int findPivot(int[] arr) {
+        int count = 0;
+        int pivot;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (arr[start] > arr[end] && start < end) {
+            count++;
+            start++;
+        }
+        pivot = count - 1;
+
+        return pivot;
+    }
+}
