698605b0640645826f916ce0b4784ee17a3e1b8f
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-10 16:53:12 +0530

    Single number in array

---
+++ LeetCode/src/P136.java
@@ -0,0 +1,14 @@
+public class P136 {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1};
+        System.out.println(singleNumber(arr));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int sum = 0;
+        for (int value : nums) {
+            sum ^= value;
+        }
+        return sum;
+    }
+}
