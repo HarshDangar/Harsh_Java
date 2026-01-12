ef2d572c8111a3e617377d7b718c87836671793e
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-12 10:26:20 +0530

    Hamming Distance

---
+++ LeetCode/src/P461.java
@@ -0,0 +1,19 @@
+public class P461 {
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1, 3));
+    }
+
+    public static int hammingDistance(int x, int y) {
+        int count = 0;
+
+        while (x != 0 || y != 0) {
+            if ((x & 1) != (y & 1))
+                count++;
+
+            x = x >> 1;
+            y = y >> 1;
+        }
+
+        return count;
+    }
+}
