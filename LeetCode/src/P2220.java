6874f870b254f7914a19aadb0141b2f1b432944d
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-12 10:26:37 +0530

    Min Bit Flips

---
+++ LeetCode/src/P2220.java
@@ -0,0 +1,19 @@
+public class P2220 {
+    public static void main(String[] args) {
+        System.out.println(minBitFlips(3, 4));
+    }
+
+    public static int minBitFlips(int start, int goal) {
+        int count = 0;
+
+        while (start != 0 || goal != 0) {
+            if ((start & 1) != (goal & 1))
+                count++;
+
+            start = start >> 1;
+            goal = goal >> 1;
+        }
+
+        return count;
+    }
+}
