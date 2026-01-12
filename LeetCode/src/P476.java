1ce1e5dcc1245bc820d90463cf048fec689031a1
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-12 10:27:14 +0530

    Find Complement

---
+++ LeetCode/src/P476.java
@@ -0,0 +1,18 @@
+public class P476 {
+    public static void main(String[] args) {
+        System.out.println(findComplement(1));
+    }
+
+    public static int findComplement(int num) {
+        int count = 0;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += (int) (((num & 1) ^ 1) * Math.pow(2, count));
+            count++;
+            num >>= 1;
+        }
+
+        return sum;
+    }
+}
