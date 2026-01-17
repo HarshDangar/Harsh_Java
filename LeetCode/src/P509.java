aa5c0216708282d0c2ee2a6e415b1170fdfa186f
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-17 09:23:48 +0530

    Fibonacci

---
+++ LeetCode/src/P509.java
@@ -0,0 +1,21 @@
+public class P509 {
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+    }
+
+    public static int fib(int n) {
+        if (n <= 1) return n;
+
+        int prev1 = 1;
+        int prev2 = 0;
+        int cur = 0;
+
+        for (int i = 2; i <= n; i++) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return cur;
+    }
+}
