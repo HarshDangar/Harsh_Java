0861364c3a2b2a23e69d57bce85cd7242c5564c3
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-17 09:24:04 +0530

    Find in mountain array

---
+++ LeetCode/src/P1095.java
@@ -0,0 +1,60 @@
+public class P1095 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 3, 1};
+        int target = 3;
+        System.out.println(findInMountainArray(target, arr));
+    }
+
+    public static int findInMountainArray(int target, int[] mountainArr) {
+        int peak = peakIndexInMountainArray(mountainArr);
+        int firstTry = orderAgnosticBS(mountainArr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        return orderAgnosticBS(mountainArr, target, peak + 1, mountainArr.length - 1);
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
