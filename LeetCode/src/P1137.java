d5317a30e7458b475db0467a24d983a306c2ccf8
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-01-17 09:24:14 +0530

    Tribonacci

---
+++ LeetCode/src/P1137.java
@@ -0,0 +1,24 @@
+public class P1137 {
+    public static void main(String[] args) {
+        System.out.println(tribonacci(25));
+    }
+
+    public static int tribonacci(int n) {
+        if (n <= 1) return n;
+        if (n == 2) return 1;
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int cur = 0;
+
+        for (int i = 3; i <= n; i++) {
+            cur = a + b + c;
+            a = b;
+            b = c;
+            c = cur;
+        }
+
+        return cur;
+    }
+}
