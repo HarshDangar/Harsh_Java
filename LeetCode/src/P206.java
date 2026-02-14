11b698f9192918cf5e7fd6387a22e3e2d327ba64
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-02-14 21:51:18 +0530

    Reverse the list

--- LeetCode/src/P206.java
+++ LeetCode/src/P206.java
@@ -16,15 +16,45 @@ public class P206 {
         }
     }
 
-    private ListNode reverseList(ListNode head) {
-        return reverseListInt(head, null);
+    private static ListNode reverseList(ListNode head) {
+        ListNode ans = null;
+        while (head != null) {
+            ListNode node = new ListNode(head.val);
+            node.next = ans;
+            ans = node;
+            head = head.next;
+        }
+        return ans;
     }
 
-    private ListNode reverseListInt(ListNode head, ListNode newHead) {
-        if (head == null)
-            return newHead;
-        ListNode next = head.next;
-        head.next = newHead;
-        return reverseListInt(next, head);
+    //Iterative method with three pointer
+    private static ListNode reverseList2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        reverseList2(head);
     }
 }
