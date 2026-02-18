1e401ac442ac03b45b05d80973f6c587fa2db54b
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-02-18 10:31:05 +0530

    Queue

--- StacksNQueues/src/QueueMain.java
+++ StacksNQueues/src/QueueMain.java
@@ -1,26 +1,6 @@
-import java.util.Queue;
-
 public class QueueMain {
     public static void main(String[] args) throws Exception{
-//        CustomQueue queue = new CustomQueue(5);
-
-//        queue.insert(4);
-//        queue.insert(5);
-//        queue.insert(6);
-//        queue.insert(8);
-//        queue.insert(9);
-//
-//        queue.display();
-//
-//        System.out.println(queue.remove());
-//
-//        queue.display();
-//
-//        queue.insert(55);
-//        queue.display();
-
-
-        CircularQueue queue = new CircularQueue(5);
+        CustomQueue queue = new CustomQueue(5);
 
         queue.insert(4);
         queue.insert(5);
@@ -30,8 +10,26 @@ public class QueueMain {
 
         queue.display();
 
-        queue.remove();
+        System.out.println(queue.remove());
 
         queue.display();
+
+        queue.insert(55);
+        queue.display();
+
+
+        CircularQueue queue2 = new CircularQueue(5);
+
+        queue2.insert(4);
+        queue2.insert(5);
+        queue2.insert(6);
+        queue2.insert(8);
+        queue2.insert(9);
+
+        queue2.display();
+
+        queue2.remove();
+
+        queue2.display();
     }
 }
