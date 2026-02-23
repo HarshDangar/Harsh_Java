666d91eaba20193ae20184b3dcf6b3b4140a98dd
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-02-23 15:40:40 +0530

    Level order successor

--- Trees/src/BFSnDFS/BFS.java
+++ Trees/src/BFSnDFS/BFS.java
@@ -76,4 +76,34 @@ public class BFS {
         }
         return result;
     }
+
+    /*Level order successor
+        Meaning we need the right node of the node given in the binary tree
+        If there is no right node in that level than return next node in the next level
+     */
+    public TreeNode findSuccessor(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode currentNode = queue.poll();
+
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+
+            if (currentNode.val == key) {
+                break;
+            }
+        }
+        return queue.peek();
+    }
 }
