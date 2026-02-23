3ef3a41d8ed0507d5061fe85fff924343f80edcb
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-02-23 15:01:57 +0530

    BFS

---
+++ Trees/src/BFSnDFS/BFS.java
@@ -0,0 +1,79 @@
+package BFSnDFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BFS {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static TreeNode sortedArrayToBST(int[] nums) {
+        return populatedSorted(nums, 0, nums.length);
+    }
+
+    private static TreeNode populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = populatedSorted(nums, start, mid);
+        root.right = populatedSorted(nums, mid + 1, end);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-10, -3, 0, 5, 9};
+        TreeNode root = sortedArrayToBST(nums);
+        System.out.println(bfs(root));
+    }
+
+    private static List<List<Integer>> bfs(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
