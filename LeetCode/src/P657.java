2521a09dcf1363018e4c1465bdc1e31f67729e2b
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-03-11 11:01:05 +0530

    judgeCircle better approach

--- LeetCode/src/P657.java
+++ LeetCode/src/P657.java
@@ -7,24 +7,12 @@ public class P657 {
     private static boolean judgeCircle(String moves) {
         int row = 0, col = 0;
 
-        if (moves.length() % 2 != 0) {
-            return false;
-        } else {
-            for (int i = 0; i < moves.length(); i++) {
-                switch (moves.charAt(i)) {
-                    case 'U':
-                        col--;
-                        break;
-                    case 'D':
-                        col++;
-                        break;
-                    case 'L':
-                        row--;
-                        break;
-                    case 'R':
-                        row++;
-                        break;
-                }
+        for (char c: moves.toCharArray()) {
+            switch (c) {
+                case 'U' -> col--;
+                case 'D'-> col++;
+                case 'L'-> row--;
+                case 'R'-> row++;
             }
         }
 
