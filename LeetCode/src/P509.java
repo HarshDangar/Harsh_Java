1fa8aae2396892a0d83d97a330e19c6988fc86e4
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-03-11 09:11:12 +0530

    More methods for Fibonacci

--- LeetCode/src/P509.java
+++ LeetCode/src/P509.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class P509 {
     public static void main(String[] args) {
-        System.out.println(fib(4));
+        System.out.println(fib(7));
+        System.out.println(fibRec(7));
+        System.out.println(fibDp(7));
+        System.out.println(fibDPMem(7));
     }
 
     private static int fib(int n) {
@@ -17,5 +22,59 @@ public class P509 {
         }
 
         return cur;
+    }
+
+    //Recursive approach
+    private static int fibRec(int n) {
+        if (n <= 1)
+            return n;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    //Dynamic Programming
+    private static int fibDp(int n) {
+        if (n <= 1)
+            return n;
+
+        int[] fib = new int[n + 1];
+
+        fib[0] = 1;
+        fib[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[n - 1];
+    }
+
+    //Dynamic programming with memoization
+    static int[] dp = new int[31];
+    static {
+        Arrays.fill(dp, -1);
+    }
+
+    private static int fibDPMem(int n) {
+        if (n <= 1)
+            return n;
+
+        // Temporary variables to store values of fib(n-1) & fib(n-2)
+        int first, second;
+
+        if (dp[n - 1] != -1) {
+            first = dp[n - 1];
+        } else {
+            first = fib(n - 1);
+        }
+
+        if (dp[n - 2] != -1) {
+            second = dp[n - 2];
+        } else {
+            second = fib(n - 2);
+        }
+
+        // Memoization
+        return dp[n] = first + second;
     }
 }
