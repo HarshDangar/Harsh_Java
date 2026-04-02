6bb0b1dc3421354933dbae27dcc54f9d36e5dca3
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-02 17:41:54 +0530

    checkStrings

---
+++ LeetCode/src/P2840.java
@@ -0,0 +1,28 @@
+public class P2840 {
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "cdab";
+        System.out.println(checkStrings(s1, s2));
+    }
+
+    private static boolean checkStrings(String s1, String s2) {
+        int[] even = new int[26];
+        int[] odd = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (i % 2 == 0) {
+                even[s1.charAt(i) - 'a']++;
+                even[s2.charAt(i) - 'a']--;
+            } else {
+                odd[s1.charAt(i) - 'a']++;
+                odd[s2.charAt(i) - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (even[i] != 0 || odd[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
