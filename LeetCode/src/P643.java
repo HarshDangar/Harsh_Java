08a6b4ebe9bfd63f20529a9f1d99c1666e9dd221
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-08 19:18:14 +0530

    findMaxAverage

---
+++ LeetCode/src/P643.java
@@ -0,0 +1,23 @@
+public class P643 {
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,3,3};
+        int k = 4;
+        System.out.println(findMaxAverage(nums, k));
+    }
+
+    private static double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+        return (double) maxSum / k;
+    }
+}
