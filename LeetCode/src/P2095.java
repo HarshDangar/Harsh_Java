061ac84f2a3c7a48f139d1460a34c9c047560536
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-09 18:10:05 +0530

    Delete Middle Node

---
+++ LeetCode/src/P2095.java
@@ -0,0 +1,45 @@
+public class P2095 {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        deleteMiddle(head);
+    }
+}
