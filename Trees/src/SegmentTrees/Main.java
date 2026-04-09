d8aecb3e7498f9f2ee3f366dc8e7445417defb5f
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-09 18:09:34 +0530

    Segment Tree

--- Trees/src/SegmentTrees/Main.java
+++ Trees/src/SegmentTrees/Main.java
@@ -5,5 +5,7 @@ public class Main {
         int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
         SegmentTree tree = new SegmentTree(arr);
         tree.display();
+
+        System.out.println(tree.query(2, 6));
     }
 }
