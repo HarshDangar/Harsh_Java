b8944669dca8eb6146f8537b6c7bff4c213f16bf
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-11 10:51:58 +0530

    Sort By Bits

--- LeetCode/src/P1356.java
+++ LeetCode/src/P1356.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class P1356 {
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-        System.out.println(Arrays.toString(sortByBits(nums)));
+        System.out.println(Arrays.toString(sortByBits2(nums)));
     }
 
     private static int[] sortByBits(int[] arr) {
@@ -15,6 +15,29 @@ public class P1356 {
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] % 10001;
+        }
+        return arr;
+    }
+
+    private static int[] sortByBits2(int[] arr) {
+        Integer[] arr2 = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[i];
+        }
+
+        Arrays.sort(arr2, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+
+            if (countA == countB)
+                return a.compareTo(b);
+
+            return Integer.compare(countA, countB);
+            //  If countA < countB -> compare will return negative -1 meaning ascending order
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr2[i];
         }
         return arr;
     }
