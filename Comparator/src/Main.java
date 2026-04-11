1d332a011b12f4747876ca70af2043ee744199c1
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-11 10:52:10 +0530

    Comparator

---
+++ Comparator/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class MyComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(2);
+
+        list.sort(new MyComparator());
+        System.out.println(list);
+    }
+}
