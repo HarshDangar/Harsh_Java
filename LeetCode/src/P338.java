0dc298721335f0f4410ce50e5504d7d6749a3e51
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-04-11 10:51:48 +0530

    Count Bits

--- LeetCode/src/P338.java
+++ LeetCode/src/P338.java
@@ -29,7 +29,18 @@ public class P338 {
     private static int[] countBits2(int n) {
         int[] ans = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+            ans[i] = ans[i >> 1] + (i & 1); //i >> 1 == i / 2 + last bit if no is odd than add 1 if even than add 0
+        }
+        return ans;
+    }
+
+    private static int[] countBits3(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1)
+                ans[i] = ans[i / 2] + 1;
+            else
+                ans[i] = ans[i / 2];
         }
         return ans;
     }
