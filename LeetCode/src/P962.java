23c943578279c20f810eb8cec78c93a6a880dcf0
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-04 13:06:29 +0530

    maxRamp - Monotonic Stack

--- LeetCode/src/P962.java
+++ LeetCode/src/P962.java
@@ -1,7 +1,12 @@
+import java.util.Stack;
+
 public class P962 {
     public static void main(String[] args) {
         int[] nums = {9, 8, 1, 0, 1, 9, 4, 0, 4, 1};
         System.out.println(maxWidthRamp(nums));
+        System.out.println(maxWidthRamp2(nums));
+        System.out.println(maxWidthRamp3(nums));
+        System.out.println(maxWidthRamp4(nums));
     }
 
     //Brute Force - TLE
@@ -57,6 +62,33 @@ public class P962 {
             ramp = Integer.max(ramp, j - i);
             j++;
         }
+        return ramp;
+    }
+
+    //Monotonic Stack : O(2 * n) == O(n)
+    private static int maxWidthRamp4(int[] nums) {
+        int n  = nums.length;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[i] <= nums[stack.peek()]) {
+                stack.push(i);
+            }
+        }
+        
+        int ramp = 0;
+
+        int j = n - 1;
+        while (j >= 0) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[j]) {
+                int i = stack.peek();
+                ramp = Math.max(ramp, j - i);
+                stack.pop();
+            }
+            j--;
+        }
+
         return ramp;
     }
 }
