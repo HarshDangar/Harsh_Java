0d901a0c308bc6f6d17274886c2ab59b64b93899
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-05 16:46:03 +0530

    partition List

---
+++ LeetCode/src/P86.java
@@ -0,0 +1,43 @@
+public class P86 {
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private static ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+
+        ListNode smallP = small;
+        ListNode largeP = large;
+
+        while (head != null) {
+            if (head.val < x) {
+                smallP.next = head;
+                smallP = smallP.next;
+            } else {
+                largeP.next = head;
+                largeP = largeP.next;
+            }
+
+            head = head.next;
+        }
+
+        smallP.next = large.next;
+        largeP.next = null;
+
+        return small.next;
+    }
+}
