668e1153ee95d23817e23b4c7ca409330fa7dcdf
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-08 15:55:49 +0530

    pow(x, n)

--- LeetCode/src/P50.java
+++ LeetCode/src/P50.java
@@ -6,19 +6,15 @@ public class P50 {
     }
 
     private static double myPow(double x, int n) {
-        return helper(x, n);
-    }
-
-    private static double helper(double x, long n) {
         if (n == 0)
             return 1;
 
         if (n < 0)
-            return helper(1 / x, -n);
+            return myPow(1 / x, -n);
 
         if (n % 2 == 0) //x^n if n is even than we can write like this x ^ 8 == (x ^ 2) ^ 4 == (x * x) ^ n/2
-            return helper(x * x, n / 2);
+            return myPow(x * x, n / 2);
 
-        return x * helper(x * x, (n - 1) / 2);
+        return x * myPow(x * x, (n - 1) / 2);
     }
 }
