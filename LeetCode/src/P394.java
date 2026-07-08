c82fc010085c1fcb1ecfee57e691dde0f89643e3
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-08 15:55:35 +0530

    Encode String added in decode string question

--- LeetCode/src/P394.java
+++ LeetCode/src/P394.java
@@ -4,6 +4,8 @@ public class P394 {
     public static void main(String[] args) {
         String s = "3[a2[c]]";
         System.out.println(decodeString(s));
+
+        System.out.println(encodeString("abcef"));
     }
 
     private static String decodeString(String s) {
@@ -34,5 +36,31 @@ public class P394 {
             }
         }
         return curr_str.toString();
+    }
+
+    //Encode String with RLE (Run Length Encoding)
+    private static String encodeString(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(count);
+                result.append(s.charAt(i - 1));
+                count = 1;
+            }
+        }
+
+        result.append(count);
+        result.append(s.charAt(s.length() - 1));
+
+        return result.toString();
     }
 }
