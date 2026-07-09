815a7c0c8adcd8bf1048150674053b3a86722b91
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-09 13:57:28 +0530

    Trapping Rain Water

---
+++ LeetCode/src/P42.java
@@ -0,0 +1,43 @@
+public class P42 {
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(trap(height));
+    }
+
+    private static int trap(int[] height) {
+        int n = height.length;
+
+        int[] leftMax = getLeftMax(height, n);
+        int[] rightMax = getRightMax(height, n);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return sum;
+    }
+
+    private static int[] getLeftMax(int[] height, int n) {
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        return leftMax;
+    }
+
+    private static int[] getRightMax(int[] height, int n) {
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        return rightMax;
+    }
+}
