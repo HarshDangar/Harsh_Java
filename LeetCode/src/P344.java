64486bfe476864ff9b40c9ee060d95d94b21fee8
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-11 22:25:35 +0530

    Reverse String

---
+++ LeetCode/src/P344.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class P344 {
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    private static void reverseString(char[] s) {
+        int start = 0, end = s.length - 1;
+        while (start < end) {
+            char temp = s[start];
+            s[start++] = s[end];
+            s[end--] = temp;
+        }
+    }
+}
