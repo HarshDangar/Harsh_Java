3adc22ad40f9c84db959c11e65a4c53e225edd38
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-14 20:15:04 +0530

    sortColors

--- LeetCode/src/P75.java
+++ LeetCode/src/P75.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class P75 {
     public static void main(String[] args) {
         int[] nums = {2, 0, 2, 1, 1, 0};
-        sortColors(nums);
+        sortColors2(nums);
         System.out.println(Arrays.toString(nums));
     }
 
@@ -31,6 +31,33 @@ public class P75 {
 
         for (int i = 0; i < nums.length; i++) {
             nums[i] = arr[i];
+        }
+    }
+
+    //Approach 2 : Dutch National Flag Algorithm
+    private static void sortColors2(int[] nums) {
+        int start = 0, mid = 0, end = nums.length - 1;
+
+        while (mid <= end) {
+            //If current ele is 0 than swap with start and move both start and mid
+            if (nums[mid] == 0) {
+                int temp = nums[start];
+                nums[start] = nums[mid];
+                nums[mid] = temp;
+
+                start++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                //If mid is 1 than it is at correct position so just move mid
+                mid++;
+            } else {
+                //If the element is 2 than swap with end and move end
+                int temp = nums[mid];
+                nums[mid] = nums[end];
+                nums[end] = temp;
+
+                end--;
+            }
         }
     }
 }
