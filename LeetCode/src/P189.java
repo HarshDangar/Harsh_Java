c42363ae4d2f8f8786f727cd461b84ac36e2a716
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-07-14 18:51:14 +0530

    rotate the array k times

--- LeetCode/src/P189.java
+++ LeetCode/src/P189.java
@@ -2,22 +2,27 @@ import java.util.Arrays;
 
 public class P189 {
     public static void main(String[] args) {
-        int[] nums = {1};
-        rotate(nums, 0);
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
         System.out.println(Arrays.toString(nums));
     }
 
     private static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        int[] rotated = new int[n];
+        k = k % nums.length;
 
-        for (int i = 0; i < n; i++) {
-            rotated[(i + k) % n] = nums[i];
-        }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = rotated[i];
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
         }
     }
 }
