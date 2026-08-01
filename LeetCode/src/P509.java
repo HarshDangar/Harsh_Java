b086195bb2cc30ae729651244854b27ae006991b
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-01 20:30:52 +0530

    Fibonacci With DP

--- LeetCode/src/P509.java
+++ LeetCode/src/P509.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class P509 {
     public static void main(String[] args) {
-        System.out.println(fib(7));
-        System.out.println(fibRec(7));
-        System.out.println(fibDp(7));
-        System.out.println(fibDPMem(7));
+        System.out.println(fib(5));
+        System.out.println(fibRec(5));
+        System.out.println(fib2(5));
+        System.out.println(fib3(5));
     }
 
     private static int fib(int n) {
@@ -32,49 +32,38 @@ public class P509 {
         return fib(n - 1) + fib(n - 2);
     }
 
-    //Dynamic Programming
-    private static int fibDp(int n) {
-        if (n <= 1)
-            return n;
-
-        int[] fib = new int[n + 1];
-
-        fib[0] = 1;
-        fib[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-
-        return fib[n - 1];
-    }
-
-    //Dynamic programming with memoization
-    static int[] dp = new int[31];
-    static {
+    //Recursion + Memoization
+    private static int fib2(int n) {
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
+        return solve(n, dp);
     }
 
-    private static int fibDPMem(int n) {
-        if (n <= 1)
+    private static int solve(int n, int[] dp) {
+        if(n <= 1)
             return n;
 
-        // Temporary variables to store values of fib(n-1) & fib(n-2)
-        int first, second;
+        if(dp[n] != -1)
+            return dp[n];
 
-        if (dp[n - 1] != -1) {
-            first = dp[n - 1];
-        } else {
-            first = fib(n - 1);
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    //Bottom up DP
+    private static int fib3(int n) {
+        if(n <= 1)
+            return n;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        if (dp[n - 2] != -1) {
-            second = dp[n - 2];
-        } else {
-            second = fib(n - 2);
-        }
-
-        // Memoization
-        return dp[n] = first + second;
+        return dp[n];
     }
 }
