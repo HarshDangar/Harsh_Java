d07c644bbff61f06d27f3aad9a16ed1ceb7cd3c8
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-01 13:38:37 +0530

    Follow up for Kaden's Algo

--- LeetCode/src/P53.java
+++ LeetCode/src/P53.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+
 public class P53 {
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = {-2, 1, -3, 4, -1, 2, -8, -5, -6};
         System.out.println(maxSubArray(arr));
         System.out.println(maxSubArray2(arr));
+        System.out.println(Arrays.toString(subArrayPrint(arr)));
     }
 
     private static int maxSubArray(int[] nums) {
@@ -20,7 +23,6 @@ public class P53 {
     }
 
     // Kaden's algorithm
-
     private static int maxSubArray2(int[] nums) {
         int maxSum = nums[0];
         int sum = nums[0];
@@ -40,4 +42,29 @@ public class P53 {
         return maxSum;
     }
 
+    //Follow up Question : Print the subarray that has maximum sum
+    private static int[] subArrayPrint(int[] nums) {
+        int maxSum = nums[0];
+        int sum = nums[0];
+
+        int start = 0;
+        int end = 0;
+        int tempStart = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (sum >= 0) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+                tempStart = i;
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                start = tempStart;
+                end = i;
+            }
+        }
+        return Arrays.copyOfRange(nums, start, end + 1);
+    }
 }
