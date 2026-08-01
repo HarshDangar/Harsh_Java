3638617140661534b485ce99335c1bfbc8a3bb30
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-01 20:31:13 +0530

    Climbing Stairs With DP

--- LeetCode/src/P70.java
+++ LeetCode/src/P70.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class P70 {
     public static void main(String[] args) {
-        System.out.println(climbStairs(5));
+        System.out.println(climbStairs(45));
+        System.out.println(climbStairs(45));
     }
 
     private static int climbStairs(int n) {
@@ -17,5 +20,27 @@ public class P70 {
         }
 
         return cur;
+    }
+
+    //Recursion + Memoization
+    static int[] dp;
+    private static int climpStairs2(int n) {
+        dp = new int[n + 1];
+        Arrays.fill(dp, -2);
+        return solve(n);
+    }
+
+    private static int solve(int n) {
+        if(n == 0) {
+            return 1;
+        }
+
+        if (n < 0)
+            return 0;
+
+        if (dp[n] != -2)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
     }
 }
