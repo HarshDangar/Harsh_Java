3e6590df1b228551d959f2773a06a93000a3abe9
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-05 15:21:34 +0530

    removeNthNode from End

--- LeetCode/src/P19.java
+++ LeetCode/src/P19.java
@@ -77,4 +77,14 @@ public class P19 {
         }
         return l;
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        removeNthFromEnd2(head, 2);
+    }
 }
