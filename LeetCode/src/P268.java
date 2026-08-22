bd4661d0e4a33f91b84b1e5a4cfe83e2125956d1
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-22 22:11:02 +0530

    missing Number

--- LeetCode/src/P268.java
+++ LeetCode/src/P268.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class P268 {
     public static void main(String[] args) {
         int[] arr = {3, 0, 1};
         System.out.println(MissingNumber(arr));
+        System.out.println(MissingNumber2(arr));
+        System.out.println(MissingNumber3(arr));
     }
 
     private static int MissingNumber(int[] nums) {
@@ -28,5 +32,33 @@ public class P268 {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
+    }
+
+    private static int MissingNumber2(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < n; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
+
+    private static int MissingNumber3(int[] nums) {
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums.length - 1;
+        int result = nums.length;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > mid) {
+                result = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return result;
     }
 }
