839483e683ed53f6f5d461f6244e3d6bf3f358a0
HarshDangar <137979647+HarshDangar@users.noreply.github.com>
2026-08-26 22:12:17 +0530

    subsetXORSum

--- LeetCode/src/P1863.java
+++ LeetCode/src/P1863.java
@@ -1,9 +1,11 @@
 public class P1863 {
     public static void main(String[] args) {
-        int[] nums = {5,1,6};
+        int[] nums = {5, 1, 6};
         System.out.println(subsetXORSum(nums));
+        System.out.println(subsetXORSum2(nums));
     }
 
+    //Approach 1
     private static int subsetXORSum(int[] nums) {
         return helper(nums, 0, 0); //Because 0 ^ num will be num
     }
@@ -17,5 +19,17 @@ public class P1863 {
         int right = helper(nums, index + 1, curr); // Right meaning we are excluding the element
 
         return left + right; //Ans will be left + right because we have traverse the all subsets
+    }
+
+    //Approach 2
+    private static int subsetXORSum2(int[] nums) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int num : nums) {
+            result |= num;
+        }
+
+        return result << (n - 1);
     }
 }
